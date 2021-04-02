ff66d26f6137bb4f43a9fdaf3447fa5bca0112bb
Dariusz Niemczyk <palid@phoelid.me>
2021-04-02 21:11:58 +0200

    Update jest tests

--- src/lib/components/Card/Card.test.ts
+++ src/lib/components/Card/Card.test.ts
@@ -1,9 +1,12 @@
+import { MockUnits } from '$lib/db/units';
 import { render } from '@testing-library/svelte';
 import Card from './Card.svelte';
 
 describe('<Card/>', () => {
 	it('matches snapshot', () => {
-		const results = render(Card);
+		const results = render(Card, {
+			unit: MockUnits[0],
+		});
 		expect(results.container).toMatchSnapshot();
 	});
 });
--- src/lib/components/DropTable/DropTable.test.ts
+++ src/lib/components/DropTable/DropTable.test.ts
@@ -1,9 +1,13 @@
 import { render } from '@testing-library/svelte';
 import DropTable from './DropTable.svelte';
 
+import { MockUnits } from '$lib/db/units';
+
 describe('<DropTable/>', () => {
 	it('matches snapshot', () => {
-		const results = render(DropTable);
+		const results = render(DropTable, {
+			unit: MockUnits[0],
+		});
 		expect(results.container).toMatchSnapshot();
 	});
 });
